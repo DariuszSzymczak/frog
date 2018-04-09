709ba36151299a9345399de02d24981cfe860f83
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-04-09 23:27:29 +0200

    s

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const http = require('http');
-const fs = require('fs');
-const hostname = '127.0.0.1';
 const port = 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('dupa');
-  res.end();
-});
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('Hello Node.js Server!');
+}
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+
+  console.log(`server is listening on ${port}`);
+})
