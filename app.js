7b9d3a54b4d53f7790745bdff20a5d008a4e9010
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-04-10 09:53:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = 5000;
 
 const requestHandler = (request, response) => {
   console.log(request.url);
-  response.end('Hello Node.js Server!');
+  response.end('DAwid cietszef!');
 }
 
 const server = http.createServer(requestHandler);
