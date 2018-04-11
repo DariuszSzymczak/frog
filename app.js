44100a9776e4556db9573cbf1b82416c43100acd
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-04-11 20:54:05 +0200

    working mongodb on mongoose. Server can send files and sendpost datafrom db

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.post('/send', function (req, res) {  
    res.send('ff');
 });
-// app.use('/css',express.static(__dirname + '/css'));
-// app.use('/js',express.static(__dirname + '/js'));
-// app.use('/img',express.static(__dirname + '/img'));
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/img',express.static(__dirname + '/img'));
 
 mongoose.connect('mongodb://mo7636_frog:Lewatywa1!@127.0.0.1:27017/mo7636_frog', function (err) {
  
@@ -25,17 +25,20 @@ mongoose.connect('mongodb://mo7636_frog:Lewatywa1!@127.0.0.1:27017/mo7636_frog',
 });
 var userSchema = mongoose.Schema({name: String});
 var Modeldo = mongoose.model('tests',userSchema);
-var judasz = new Modeldo({_id: new mongoose.Types.ObjectId(),name:'JUDASssZ'});
-judasz.save(function(err) {if (err) throw err;   
-  console.log('Author successfully saved.');   
-  });
-  
+// var judasz = new Modeldo({_id: new mongoose.Types.ObjectId(),name:'JUDASssZ'});
+// judasz.save(function(err) {if (err) throw err;   
+//   console.log('Author successfully saved.');   
+//   });
+app.post('/send', function(req, res) {
+  res.sendFile('generator.html', { root: __dirname });
+  Modeldo.find().exec(function(err, names) {
+   if (err) throw err;   
+   res.send(names);
+ });
+ });  
+
 app.get('/', function(req, res) {
- // res.sendFile('generator.html', { root: __dirname });
- Modeldo.find().exec(function(err, names) {
-  if (err) throw err;   
-  res.send(names);
-});
+ res.sendFile('generator.html', { root: __dirname });
 });
 
 app.listen(process.env.PORT || 9000, process.env.IP || '0.0.0.0' );
