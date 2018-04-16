6f39b2c1963f46a84f12d92f6b36a9e6609fe9a9
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-04-16 19:23:53 +0200

    kk

--- js/page.js
+++ js/page.js
@@ -60,14 +60,13 @@ $('#info_box_ok').click(function(){
     url: "http://frog.ct8.pl/send/",
     type: "post", //typ połączenia
     contentType: 'aplication/json', //gdy wysyłamy dane czasami chcemy ustawić ich typ
-    dataType: "json",
     data: {
       "name" : name_site,
       "content" : page_content
     }
   })
   .done(function (response) {
-    console.log(JSON.parse(response));
+    console.log(response);
   })
   .fail(function () {
     console.warn("Wystąpił błąd w połączniu");
