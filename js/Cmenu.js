0d3be4684a0b9b0b9c37c6d98af1a89616c0c60e
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-04-29 21:42:17 +0200

    ajax finish

--- js/Cmenu.js
+++ js/Cmenu.js
@@ -4,32 +4,35 @@ $(document).ready(function()
 });
 
 function addmenu_getPages(){
-    
+    let res;
+    $.ajax({
+        url: "http://frog.ct8.pl/pages/",
+        type: "post", //typ połączenia
+        data: {
+          "name": "getPages"
+        }
+      })
+      .done(function (response) {
+        res = response;
+        console.log(res);
+      })
+      .fail(function () {
+        console.warn("Wystąpił błąd w połączniu");
+        res = false;
+      });
+      return res;
 }
 
 function createMenu(){
     let box = create_box('addmenu_button','','','');
     let name_input = `<input type='text' id='addmenu_input_name' placeholder='podaj nazwe menu'></input>`;
     let select_input='<select id ="addmenu_input_select"></select>';
-    let page_list;
-    $.ajax({
-        url: "http://frog.ct8.pl/pages/",
-        type: "post", //typ połączenia
-        data: {"name": "getPages"}})
-      .done(function (response) {
-        page_list = response;
-        console.log(page_list);})
-      .fail(function () {
-        console.warn("Wystąpił błąd w połączniu");
-        page_list = false;
-      });
-
-
+    let page_list = addmenu_getPages();
     $('#'+box).append(name_input+select_input);
-    for(let x=0;x<page_list;x++){
-        console.log('dodaje: '+page_list[x].name);
-        $('#addmenu_input_select').append(`<option>${page_list[x].name}</option>`);
-    } 
+ for(let x=0;x<page_list.length;x++){
+            console.log('dodaje: '+page_list[x].name);
+            $('#addmenu_input_select').append(`<option>${page_list[x].name}</option>`);
+        } 
 
 
     
