59679e53d00fabef7c4953d5b673c760622e0f03
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-04-29 21:29:52 +0200

    sda

--- js/Cmenu.js
+++ js/Cmenu.js
@@ -3,36 +3,37 @@ $(document).ready(function()
   add_button_to_menu('addmenu_button','Dodaj Menu',createMenu);
 });
 
+function addmenu_getPages(){
+    let res;
+    $.ajax({
+        url: "http://frog.ct8.pl/pages/",
+        type: "post", //typ połączenia
+        data: {
+          "name": "getPages"
+        }
+      })
+      .done(function (response) {
+        res = response;
+        console.log(res);
+      })
+      .fail(function () {
+        console.warn("Wystąpił błąd w połączniu");
+        res = false;
+      });
+      return res;
+}
+
 function createMenu(){
     let box = create_box('addmenu_button','','','');
     let name_input = `<input type='text' id='addmenu_input_name' placeholder='podaj nazwe menu'></input>`;
     let select_input='<select id ="addmenu_input_select"></select>';
     let page_list = addmenu_getPages();
     $('#'+box).append(name_input+select_input);
-    if(page_list == false)alert('błąd w łączeniu z bazą danych');
-    else{
-        for(let x=0;x<page_list;x++){
-            console.log('dodaje: '+page_list[x].name)
-            $('#addmenu_input_select').append(`<option>${page_list[x].name}</option>`);
-        } 
-    }
-    function addmenu_getPages(){
-        let res;
-        $.ajax({
-            url: "http://frog.ct8.pl/pages/",
-            type: "post", //typ połączenia
-            data: {
-              "name": "getPages"
-            }
-          })
-          .done(function (response) {
-            res = response;
-            console.log(res);
-          })
-          .fail(function () {
-            console.warn("Wystąpił błąd w połączniu");
-            res = false;
-          });
-          return res;
-    }
+    for(let x=0;x<page_list;x++){
+        console.log('dodaje: '+page_list[x].name)
+        $('#addmenu_input_select').append(`<option>${page_list[x].name}</option>`);
+    } 
+
+
+    
 }
