4d143e6e69a0d6db8d62485f865bf0b8f5365a70
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-04-30 14:04:40 +0200

    asdasd

--- js/Cmenu.js
+++ js/Cmenu.js
@@ -44,8 +44,8 @@ function createMenu() {
   function addTr(direction) {
     let tr = `<tr><td colspan='3'>${$('#addmenu_input_select').val()}</td><td colspan='1'class="addmenu_delete">\
     <img src="img/x.png"/ class="css_menu_img"></td></tr>`;
-    if(direction == true) $('#addmenu_table_button_down').after(tr);
-    else $('#addmenu_table_button_up').before(tr);
+    if(direction == true) $('#addmenu_table_button_down').parent().parent().after(tr);
+    else $('#addmenu_table_button_up').parent().parent().before(tr);
     $('.addmenu_delete').on('click',function(){$(this).parent().remove()});
   }
 
