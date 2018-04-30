a1527915bc3e3c931631cd1b5cd97fb8ec8e3d77
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-04-30 13:43:15 +0200

    asd

--- js/Cmenu.js
+++ js/Cmenu.js
@@ -10,14 +10,14 @@ function addmenu_getPages() {
 
 function createMenu() {
   let box = create_box('addmenu_button', '', '', '');
-  let name_input = `<input type='text' id='addmenu_input_name' placeholder='podaj nazwe menu (Nagłówek)'></input></br>`;
+  let name_input = `<input type='text' id='addmenu_input_name' placeholder='podaj nazwe menu' style="margin-bottom: 1vw"></input></br>`;
   let select_input = '<div id="addmenu_selects" style="display:none" >\
     <label>Wybierz Stronę </label><select id ="addmenu_input_select"></select>\
     <table id="addmenu_table"></table></div>';
   let page_list = addmenu_getPages();
   $('#' + box).append(name_input + select_input);
-  $('#addmenu_table').after('<button id="addmenu_table_button_down" class="menu_long_button">+</button>'); 
-  $('#' + box).append('<button id="addmenu_create_button" class="menu_long_button">Stwórz Menu</button>');
+  $('#addmenu_table').after('<button id="addmenu_table_button_down" class="menu_long_button2">+</button>'); 
+  $('#' + box).append('<button id="addmenu_create_button" class="menu_long_button2">Stwórz Menu</button>');
   $.ajax({
       url: "http://frog.ct8.pl/pages/",
       type: "post", //typ połączenia
@@ -44,17 +44,16 @@ function createMenu() {
   function addTr(direction) {
     let tr = `<tr><td>${$('#addmenu_input_select').val()}</td><td class="css_menu_delete">\
     <img src="img/x.png"/ class="css_menu_img"></td></tr>`;
-    if(direction == 'up') $('#addmenu_table').append(tr);
-    else $('#addmenu_table').prepend(tr);
+    if(direction == 'up') $('#addmenu_table').prepend(tr);
+    else $('#addmenu_table').append(tr);
     $('.css_menu_delete').on('click',function(){$(this).parent().remove()});
   }
 
   $('#addmenu_table_button_down').click(function(){
     if($('#addmenu_table_button_up').length == 0 ){
-      $('#addmenu_table').before('<button id="addmenu_table_button_up" class="menu_long_button">+</button>');
+      $('#addmenu_table').before('<button id="addmenu_table_button_up2" class="menu_long_button">+</button>');
       $('#addmenu_table_button_up').click(addTr('up'));  
-    } 
-    addTr('');
+    } else addTr('');
   });
 
 
