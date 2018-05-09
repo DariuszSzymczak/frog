eeec678c4c01c176919e0fece2d30b945cad82c4
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-05-09 19:47:30 +0200

    sadfads

--- js/Add_Sites.js
+++ js/Add_Sites.js
@@ -6,7 +6,7 @@ function AddSite(edit_values) {
   check_to_create('addsites_button', function () {
 
     let box = create_box('addsites_button', '', '', deleteSites);
-    
+
     //list of second box items  
     let select_input = '<div id="addsites_selects">\
         <label>Wybierz Stronę </label><select id ="addsites_input_select"></select>\
@@ -16,7 +16,7 @@ function AddSite(edit_values) {
     $('#' + box).append(select_input);
     $('#addsites_table').prepend('<tr><td colspan="5"><button id="addsites_table_button_up" class="menu_long_button2">+</button></td></tr>');
     $('#addsites_table').append('<tr><td colspan="5"><button id="addsites_table_button_down" class="menu_long_button2">+</button></td></tr>');
-    let sites_json=[{}];
+    let sites_json = [{}];
     $.ajax({
         url: "http://frog.ct8.pl/pages/",
         type: "post", //typ połączenia
@@ -25,7 +25,7 @@ function AddSite(edit_values) {
         }
       })
       .done(function (page_list) {
-        
+
         for (let x = 0; x < page_list.length; x++) {
           sites_json[x] = page_list[x];
           $('#addsites_input_select').append(`<option>${page_list[x].name}</option>`);
@@ -34,37 +34,34 @@ function AddSite(edit_values) {
       .fail(function () {
         console.warn("addsites: Wystąpił błąd w połączniu");
       });
-      console.log(sites_json);
+    console.log(sites_json);
 
-   
-    // function addTr(direction, data) {
-    //   var name = $('#addmenu_input_select').val();
-    //   if (data) name = data;
-    //   let tr = `<tr><td colspan='4'>${name}</td><td colspan='1'class="addmenu_delete" data-name="${name}">\
-    //       <img src="img/x.png"/ class="css_menu_img addmenu_img"></td></tr>`;
-    //   if (direction == true) {
-    //     $('#addmenu_table_button_up').parent().parent().after(tr);
-    //     if (!data) $(`#${menu_ID}_ul`).prepend(link);
-    //   } else {
-    //     $('#addmenu_table_button_down').parent().parent().before(tr);
-    //     if (!data) $(`#${menu_ID}_ul`).append(link);
-    //   }
-    //   $(`.addmenu_delete[data-name="${name}"]`).on('click', function () {
-    //     $(this).parent().remove();
-    //     $(`#${menu_ID}_li_${name}`).remove();
-    //   });
-    // }
+    // direction -> true mean Up
+    function addTr(direction, data) {
+      var name = $('#addsites_input_select').val();
+      if (data) name = data;
+      let tr = `<tr><td colspan='4'>${name}</td><td colspan='1'class="addsites_delete" data-name="${name}">\
+          <img src="img/x.png"/ class="css_menu_img addmenu_img"></td></tr>`;
+      if (direction == true) {
+        $('#addsite_table_button_up').parent().parent().after(tr);
+      } else {
+        $('#addmenu_table_button_down').parent().parent().before(tr);
+      }
+      $(`.addsites_delete[data-name="${name}"]`).on('click', function () {
+        $(this).parent().remove();
+        $(`#${menu_ID}_li_${name}`).remove();
+      });
+    }
+    
+    addTr(false,'home');
 
-    // $('#addmenu_table_button_down').click(function () {
-    //   if ($('#addmenu_table_button_up').length == 0) {
-        
-    //     $('#addmenu_table_button_up').click(function () {
-    //       addTr(true)
-    //     });
-    //     console.log('wywolano dodaj w dol');
-    //   }
-    //   addTr('');
-    // });
+    $('#addsites_table_button_down').click(function () {
+
+      addTr(false);
+    });
+    $('#addsites_table_button_up').click(function () {
+      addTr(true)
+    });
 
     // if (edit_values) {
     //   menu_ID = target_container;
