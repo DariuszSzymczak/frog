697a42b6456728ca7352b324e0d967837e270568
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-05-14 00:12:22 +0200

    asdfsdf

--- routes/test.js
+++ routes/test.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // Home page route.
-router.post('http://frog.ct8.pl/test', function (req, res) {
+router.post('/', function (req, res) {
   res.send('Wiki home page');
 });
 module.exports = router;
