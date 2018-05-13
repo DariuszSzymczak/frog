4c5850a86745385d660b6ece870ab6609a5e9f33
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-05-13 20:42:59 +0200

    asds

--- routes/serveMain.js
+++ routes/serveMain.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var PageScheme = require('../lib/PageScheme');
+var path = require('path');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies from requests
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies from requests
+
+//add reference to directory and next resolve all names of directory to send static files
+app.use(express.static(path.resolve(__dirname, 'public'))); 
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/img', express.static(__dirname + '/img'));
 
 router.post('/pages/main', function (req, res) {
   
@@ -13,5 +23,5 @@ router.post('/pages/main', function (req, res) {
     res.status(200).json(pages);
     });
   });
-  
+
   module.exports = router;
