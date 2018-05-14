91daf7179837de5f35b746154f7aa97d12f87f45
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-05-14 18:53:30 +0200

    sdafs

--- routes/sendPage.js
+++ routes/sendPage.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var PageScheme = require('../lib/PageScheme');
+var app = express(); //use express.js as an framework in application
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+//switch on all features to work with the requests
+app.use(bodyParser.json()); // support json encoded bodies from requests
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies from requests
 
 router.post('/', function (req, res) {
   
