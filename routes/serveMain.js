cb4d98835b4b38f30814f4e3ece9d10ffce40fd5
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-05-14 18:49:21 +0200

    asdfas

--- routes/serveMain.js
+++ routes/serveMain.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var PageScheme = require('../lib/PageScheme');
-var path = require('path');
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies from requests
-app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies from requests
 
-//add reference to directory and next resolve all names of directory to send static files
-app.use(express.static(path.resolve(__dirname, 'public'))); 
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/img', express.static(__dirname + '/img'));
-
-router.post('/pages/main', function (req, res) {
+router.post('/', function (req, res) {
   
     PageScheme.findOne({'main' : true},function (err, pages) {
     if (err) {
