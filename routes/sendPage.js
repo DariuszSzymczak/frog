392a3d06f7a87f8004b7ba4b9c7474d97a4b872c
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-05-16 21:26:35 +0200

    asdfasdf

--- routes/sendPage.js
+++ routes/sendPage.js
@@ -6,46 +6,53 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 //switch on all features to work with the requests
 app.use(bodyParser.json()); // support json encoded bodies from requests
-app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies from requests
-
-   // let data_to_save = new PageScheme({
-    //   _id: new mongoose.Types.ObjectId(),
-    //   name: req.body.name,
-    //   content: req.body.content,
-    //   main: req.body.main
-    // });
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // support encoded bodies from requests
 
 
-    // data_to_save.save(function (err) {
-    //   if (err) throw err;
-    // });
-    
-    // PageScheme.find({},'name',function (err, pages) {
-    // if (err) return handleError(err);
-    // res.json(pages);
-    // });
 
 
 router.post('/', function (req, res) {
-  if(req.body.main == "true"){
+  if (req.body.main == "true") {
 
-    PageScheme.findOne({'main' : true},function (err, pages) {
+    PageScheme.findOne({
+      'main': true
+    }, function (err, pages) {
       if (err) {
         return handleError(err);
         res.status(500).send();
       }
-      if(!pages) res.status(404).send();
-      pages.main = false;
-      pages.save(function(err,newItem){
-        if(err) res.status(500).send();
-        res.json(newItem);
-      });
-      });
-    
-  }
-  else res.send(req.body.main);
+      if (pages) {
 
+        pages.main = false;
+        pages.save(function (err, newItem) {
+          if (err) res.status(500).send();
+        });
+      }
+    });
+  } 
+
+
+    let data_to_save = new PageScheme({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      content: req.body.content,
+      main: req.body.main
+    });
+
+
+    data_to_save.save(function (err) {
+      if (err) throw err;
+    });
+
+    PageScheme.find({}, 'name', function (err, pages) {
+      if (err) return handleError(err);
+      res.json(pages);
+    });
  
-  });
 
-  module.exports = router;
+
+});
+
+module.exports = router;
