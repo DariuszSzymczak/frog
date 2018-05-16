f50c21fc3c302a0ad1b93b327b2d0bcfe3446d23
Dariusz Szymczak <dariusz.szymczak96@gmail.com>
2018-05-16 20:56:47 +0200

    asdasdfasdf

--- routes/sendPage.js
+++ routes/sendPage.js
@@ -9,36 +9,14 @@ app.use(bodyParser.json()); // support json encoded bodies from requests
 app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies from requests
 
 router.post('/', function (req, res) {
-  
-
-
-    if(req.body.main == true){
-      PageScheme.findOne({'main' : true},function (err, pages) {
-        if (err) {
-          return handleError(err);
-          res.status(500).send();
-        }
-        if(!pages) res.status(404).send();
-        res.status(200).json(pages);
-        });
+  PageScheme.findOne({'main' : true},function (err, pages) {
+    if (err) {
+      return handleError(err);
+      res.status(500).send();
     }
-
-    // let data_to_save = new PageScheme({
-    //   _id: new mongoose.Types.ObjectId(),
-    //   name: req.body.name,
-    //   content: req.body.content,
-    //   main: req.body.main
-    // });
-
-
-    // data_to_save.save(function (err) {
-    //   if (err) throw err;
-    // });
-    
-    // PageScheme.find({},'name',function (err, pages) {
-    // if (err) return handleError(err);
-    // res.json(pages);
-    // });
+    if(!pages) res.status(404).send();
+    res.status(200).json(pages);
+    });
   });
 
   module.exports = router;
